adde3a08f498234c727a06177508447c5e3c3d97
Andrii <yukihirosm@gmail.com>
2023-08-03 14:47:44 +0300

    Fix urls

--- logika_administrative/urls.py
+++ logika_administrative/urls.py
@@ -23,7 +23,7 @@ from logika_general import views as logika_general_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("logika_general.urls", namespace="logika_general")),
-    path("", include("logika_auth.urls", namespace="logika_auth")),
+    path("", include("logika_auth.urls")),
     path("logika-teachers/", include("logika_teachers.urls", namespace="logika_teachers")),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
