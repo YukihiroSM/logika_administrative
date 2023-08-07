4b5502aade24467a23e6090aad94e91059d94112
Andrii <yukihirosm@gmail.com>
2023-08-07 11:51:30 +0300

    Register regional tutor profile

--- logika_teachers/admin.py
+++ logika_teachers/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from logika_teachers.models import TeacherProfile, TutorProfile, TeacherFeedback
+from logika_teachers.models import TeacherProfile, TutorProfile, TeacherFeedback, RegionalTutorProfile
 
 admin.site.register(TeacherProfile)
 admin.site.register(TutorProfile)
 admin.site.register(TeacherFeedback)
+admin.site.register(RegionalTutorProfile)
 # Register your models here.
