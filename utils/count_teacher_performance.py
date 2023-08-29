3e43dbdc8f4962c5f8f32033570d4454d8ee4363
Andrii <yukihirosm@gmail.com>
2023-08-29 12:31:18 +0300

    Add teacher performance collection and visual

--- utils/count_teacher_performance.py
+++ utils/count_teacher_performance.py
@@ -13,21 +13,20 @@ def is_lesson_in_month(lesson_date, month):
 
 def get_teacher_performance_by_month(teacher_id, locations, month):
     teacher = TeacherProfile.objects.get(id=teacher_id)
-    groups = Group.objects.filter(venue__in=locations, teacher_id=teacher.lms_id)
+    groups = Group.objects.filter(venue__in=locations, teacher_id=teacher.lms_id, type="regular")
     session = get_authenticated_session()
     results = {}
     for group in groups:
         month_performance = []
-        if group.type == "regular":
-            group_performance_resp = session.get(
-                f"https://lms.logikaschool.com/api/v2/group/lesson/index?groupId={group.lms_id}&status=active&expand=module&perPage=500"
-            )
-            if group_performance_resp.status_code == 200:
-                group_performance_data = group_performance_resp.json()["data"]["items"]
-                for lesson in group_performance_data:
-                    if lesson.get("startTime") and is_lesson_in_month(lesson.get("startTime"), month):
-                        month_performance.append(
-                            lesson.get("regularTaskScoreSumPercent") if lesson.get("regularTaskScoreSumPercent") else 0
-                        )
+        group_performance_resp = session.get(
+            f"https://lms.logikaschool.com/api/v2/group/lesson/index?groupId={group.lms_id}&status=active&expand=module&perPage=500"
+        )
+        if group_performance_resp.status_code == 200:
+            group_performance_data = group_performance_resp.json()["data"]["items"]
+            for lesson in group_performance_data:
+                if lesson.get("startTime") and is_lesson_in_month(lesson.get("startTime"), month):
+                    month_performance.append(
+                        lesson.get("regularTaskScoreSumPercent") if lesson.get("regularTaskScoreSumPercent") else 0
+                    )
         results[group.lms_id] = month_performance
     return results
