1e379ec97f5a867fa70e91b33c22b093ad951710
Andrii Smidonov <yukihirosm@gmail.com>
2024-02-26 21:22:23 +0200

    Wrong locations detection temp script

--- scripts/fixer.py
+++ scripts/fixer.py
@@ -5,15 +5,32 @@ import logging
 import datetime
 from pathlib import Path
 import json
-from logika_statistics.models import StudentReport, Location
+from logika_statistics.models import StudentReport, Location, MasterClassRecord, PaymentRecord
 from utils.lms_authentication import get_authenticated_session
 from concurrent.futures import ThreadPoolExecutor
 import pandas as pd
 
 
 def run():
-    locations_in_reports = list(set(StudentReport.objects.values_list("location")))
-    locations = list(set(Location.objects.values_list("lms_location_name")))
-    for loc in locations_in_reports:
-        if loc not in locations:
-            print(loc)
+    locations_in_mc = MasterClassRecord.objects.values_list("location", flat=True)
+    locations_in_payments = PaymentRecord.objects.values_list("location", flat=True)
+
+    locations_in_mc = list(set(locations_in_mc))
+    locations_in_payments = list(set(locations_in_payments))
+
+    real_locations = Location.objects.values_list("lms_location_name", flat=True)
+
+    print("Локации, по которым есть МК:")
+    for location in locations_in_mc:
+        if not (location in real_locations):
+            print(location)
+
+    print("Локации, по которым есть оплаты:")
+    for location in locations_in_payments:
+        if not (location in real_locations):
+            print(location)
+
+
+
+
+
