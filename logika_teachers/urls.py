44f4f35e10db422a350be0cf960a7d952e38eef5
Denis Salnikov <denissalnikov@ukr.net>
2024-08-05 19:52:48 +0300

    Code refactoring
    Added url for getting group title.
    Added url for changing churn status.
    Added url for adding new churn.

--- logika_teachers/urls.py
+++ logika_teachers/urls.py
@@ -1,70 +1,84 @@
 from django.urls import path
 
-from logika_teachers import views
+from logika_teachers.views import teacher_views, api_views, tutor_views
 
 urlpatterns = [
-    path("teacher-profile/<int:id>/", views.teacher_profile, name="teacher-profile"),
+    path("teacher-profile/<int:id>/", teacher_views.teacher_profile, name="teacher-profile"),
     path(
         "teacher-profile/<int:id>/<int:tutor_id>",
-        views.teacher_profile,
+        teacher_views.teacher_profile,
         name="teacher-profile-regional",
     ),
-    path("create-teacher/", views.create_teacher, name="create-teacher"),
+    path("create-teacher/", teacher_views.create_teacher, name="create-teacher"),
     path(
         "edit-teacher-profile/<int:id>/",
-        views.edit_teacher_profile,
+        teacher_views.edit_teacher_profile,
         name="edit-teacher-profile",
     ),
     path(
         "teacher-feedback/<int:teacher_id>/<int:tutor_id>/",
-        views.teacher_feedback_form,
+        teacher_views.teacher_feedback_form,
         name="teacher-feedback",
     ),
     path(
         "teacher-feedback-get-chur-name/",
-        views.get_churn_name,
+        api_views.get_churn_name,
         name="get-churn-name",
     ),
-    path("view-forms/<int:feedback_id>/", views.view_forms, name="view-forms"),
-    path("create-comment/", views.create_comment, name="create-comment"),
+    path(
+        "teacher-feedback-get-group-title/",
+        api_views.get_group_title,
+        name="get_group_title",
+    ),
+    path(
+        "teacher-profile-change-churn-status/",
+        api_views.change_churn_status,
+        name="change-churn-status",
+    ),
+    path(
+        "teacher-profile-add-new-churn/",
+        api_views.add_new_churn,
+        name="add-new-churn",
+    ),
+    path("view-forms/<int:feedback_id>/", teacher_views.view_forms, name="view-forms"),
+    path("create-comment/", teacher_views.create_comment, name="create-comment"),
     path(
         "refresh-credentials/<int:user_id>/",
-        views.refresh_credentials,
+        teacher_views.refresh_credentials,
         name="refresh-credentials",
     ),
     path(
         "teacher-performance/<int:teacher_id>",
-        views.teacher_performance,
+        teacher_views.teacher_performance,
         name="teacher-performance",
     ),
-    path("tutor-results/", views.tutor_results, name="tutor-results"),
     path(
         "tutor-month-report/<int:user_id>",
-        views.tutor_month_report,
+        tutor_views.tutor_month_report,
         name="tutor-month-report",
     ),
     path(
         "add-performance-to-report/<int:teacher_id>",
-        views.add_performance_to_report,
+        teacher_views.add_performance_to_report,
         name="add-performance-to-report",
     ),
     path(
-        "tutor-results-report", views.tutor_results_report, name="tutor-results-report"
+        "tutor-results-report", tutor_views.tutor_results_report, name="tutor-results-report"
     ),
-    path("unsub-teacher/<int:teacher_id>", views.unsub_teacher, name="unsub-teacher"),
+    path("unsub-teacher/<int:teacher_id>", teacher_views.unsub_teacher, name="unsub-teacher"),
     path(
         "teacher-conversion/<int:teacher_id>",
-        views.get_teacher_conversion,
+        teacher_views.get_teacher_conversion,
         name="teacher-conversion",
     ),
     path(
         "teacher-conversion/<int:teacher_id>/<int:tutor_id>",
-        views.get_teacher_conversion,
+        teacher_views.get_teacher_conversion,
         name="teacher-conversion-regional",
     ),
     path(
         "tutor-teachers-statistics/",
-        views.get_tutors_conversion,
+        tutor_views.get_tutors_conversion,
         name="tutor-teachers-statistics",
     ),
 ]
