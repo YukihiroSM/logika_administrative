76efd0cd59d41c345bbd0b009dd7019faba2b9b8
Denis Salnikov <denissalnikov@ukr.net>
2024-08-05 19:30:42 +0300

    Fixed function names
    Now exists two functions for getting Group objects and only titles.

--- utils/get_teacher_groups.py
+++ utils/get_teacher_groups.py
@@ -1,15 +1,23 @@
+from django.db.models import QuerySet
+
 from logika_statistics.models import Group
 from logika_teachers.models import TeacherProfile
+from typing import Union
 
 
-def get_teacher_groups(teacher_id):
+def get_teacher_group_titles(teacher_id) -> QuerySet[str]:
     teacher = TeacherProfile.objects.get(id=teacher_id)
-    locations = (
-        Group.objects.filter(
+    groups = Group.objects.filter(
             teacher_id=teacher.lms_id,
             type__in=("regular", "individual", "Группа", "Индивидуальная"),
         )
-        .values_list("title", flat=True)
-        .distinct()
-    )
-    return locations
+    return groups.values_list("title", flat=True).distinct()
+
+
+def get_teacher_groups(teacher_id) -> QuerySet[Group]:
+    teacher = TeacherProfile.objects.get(id=teacher_id)
+    groups = Group.objects.filter(
+            teacher_id=teacher.lms_id,
+            type__in=("regular", "individual", "Группа", "Индивидуальная"),
+        )
+    return groups.distinct()
