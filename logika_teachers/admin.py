7799291998627efe5484822f1fe0f587cf9dcef6
Denis Salnikov <denissalnikov@ukr.net>
2024-08-07 21:39:53 +0300

    Updated tutor change page
    Added inline field for related teachers.

--- logika_teachers/admin.py
+++ logika_teachers/admin.py
@@ -7,11 +7,22 @@ from logika_teachers.models import (
 )
 
 
+class RelatedTeachersInline(admin.TabularInline):
+    model = TeacherProfile.related_tutors.through
+    extra = 0
+    autocomplete_fields = ["teacherprofile"]
+
+
 class TeacherProfileAdmin(admin.ModelAdmin):
     search_fields = ["user__last_name", "user__first_name"]
 
 
+class TutorProfileAdmin(admin.ModelAdmin):
+    search_fields = ["user__last_name", "user__first_name"]
+    inlines = [RelatedTeachersInline]
+
+
 admin.site.register(TeacherProfile, TeacherProfileAdmin)
-admin.site.register(TutorProfile)
+admin.site.register(TutorProfile, TutorProfileAdmin)
 admin.site.register(TeacherFeedback)
 admin.site.register(RegionalTutorProfile)
