57caf8b08d392ccd5ff1f0570d83e18da935ba4a
Denis Salnikov <denissalnikov@ukr.net>
2024-08-11 02:06:07 +0300

    Added: display of predicted churns on the main page
    — Predicted churn display added for tutors only.
    — Predicted churn is displayed on the main page before the list of teachers.
    — Added the ability to edit predicted churn information in the churn interface.
    — Added the ability to view all comments related to a specific churn.

--- logika_general/views.py
+++ logika_general/views.py
@@ -1,12 +1,15 @@
+from datetime import datetime, timedelta
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, redirect
 
 from logika_teachers.models import (
     TeacherProfile,
     TutorProfile,
     TeacherFeedback,
-    RegionalTutorProfile,
+    RegionalTutorProfile, PredictedChurn, TeacherComment,
 )
 from utils.get_user_role import get_user_role
 
@@ -17,6 +20,11 @@ def index(request):
     teachers = []
     tutors = []
     feedbacks = []
+    predicted_churns = []
+    churn_comments = []
+    from_date = ""
+    to_date = ""
+    teacher_name = ""
     if user_role == "teacher":
         teacher_profile = TeacherProfile.objects.filter(user=request.user).first()
         feedbacks = (
@@ -26,6 +34,17 @@ def index(request):
         )
         tutors = teacher_profile.related_tutors.all()
     if user_role == "tutor":
+        if request.method == "POST":
+            churn_pk = request.POST.get("churn_pk")
+            if churn_pk:
+                status = request.POST.get("status", "relevant")
+                description = request.POST.get("description", "")
+                priority = request.POST.get("priority", 0)
+                churn = PredictedChurn.objects.get(pk=churn_pk)
+                churn.status = status
+                churn.description = description
+                churn.priority = priority
+                churn.save()
         teachers = (
             TutorProfile.objects.filter(user=request.user)
             .first()
@@ -39,6 +58,31 @@ def index(request):
                 .order_by("-created_at")
                 .first()
             )
+        teacher_name = request.GET.get("churn_teacher", "")
+        if teacher_name:
+            churn_teachers = teachers.filter(
+                (Q(user__first_name__icontains=teacher_name) | Q(user__last_name__icontains=teacher_name))
+            )
+        else:
+            churn_teachers = teachers
+        predicted_churns = PredictedChurn.objects.filter(teacher__in=churn_teachers).order_by("-priority", "-created_at")
+
+        status = request.GET.get("churn_status")
+        if status:
+            predicted_churns = predicted_churns.filter(status=status)
+
+        from_date = request.GET.get("churn_date_from")
+        from_date = datetime.strptime(from_date, "%Y-%m-%d") if from_date else None
+        to_date = request.GET.get("churn_date_to")
+        to_date = datetime.strptime(to_date, "%Y-%m-%d") if to_date else None
+        if from_date:
+            predicted_churns = predicted_churns.filter(created_at__gte=from_date)
+        if to_date:
+            predicted_churns = predicted_churns.filter(created_at__lte=to_date)
+        tutor_comments = TeacherComment.objects.filter(tutor=tutor_profile)
+        churn_comments = []
+        for churn in PredictedChurn.objects.filter(teacher__in=teachers):
+            churn_comments.append((churn.churn_id, tutor_comments.filter(churn_id=churn.churn_id)))
     if user_role == "regional_tutor" or user_role == "admin":
         regional_tutor_profile = RegionalTutorProfile.objects.filter(
             user=request.user
@@ -51,13 +95,18 @@ def index(request):
 
     return render(
         request,
-        "index.html",
+        "index/index.html",
         context={
             "user": request.user,
             "user_role": user_role,
             "tutors": tutors,
             "teachers": teachers,
             "feedbacks": feedbacks,
+            "predicted_churns": predicted_churns,
+            "churn_comments": churn_comments,
+            "from_date": from_date.strftime("%Y-%m-%d") if from_date else "",
+            "to_date": to_date.strftime("%Y-%m-%d") if to_date else "",
+            "teacher_name": teacher_name,
         },
     )
 
