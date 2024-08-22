a1d6f00136fbb64e75a42404d33ec91a7e0f4f4b
Denis Salnikov <denissalnikov@ukr.net>
2024-08-22 21:13:04 +0300

    Optimize: open lessons and churns for tutors
    Now open lessons and churn (include comments for both) getting from js fetch requests to new API endpoints.

--- logika_general/views.py
+++ logika_general/views.py
@@ -29,7 +29,7 @@ def index(request):
     to_date = ""
     teacher_name = ""
     les_from_date = datetime.now()
-    les_to_date = datetime.now()
+    les_to_date = datetime.now() + timedelta(days=7)
     open_lessons = []
     if user_role == "teacher":
         teacher_profile = TeacherProfile.objects.filter(user=request.user).first()
@@ -64,56 +64,6 @@ def index(request):
                 .order_by("-created_at")
                 .first()
             )
-        teacher_name = request.GET.get("churn_teacher", "")
-        if teacher_name:
-            churn_teachers = teachers.filter(
-                (Q(user__first_name__icontains=teacher_name) | Q(user__last_name__icontains=teacher_name))
-            )
-        else:
-            churn_teachers = teachers
-        predicted_churns = PredictedChurn.objects.filter(teacher__in=churn_teachers).order_by("-priority", "-created_at")
-
-        status = request.GET.get("churn_status")
-        if status:
-            predicted_churns = predicted_churns.filter(status=status)
-
-        from_date = request.GET.get("churn_date_from")
-        from_date = datetime.strptime(from_date, "%Y-%m-%d") if from_date else None
-        to_date = request.GET.get("churn_date_to")
-        to_date = datetime.strptime(to_date, "%Y-%m-%d") if to_date else None
-        if from_date:
-            predicted_churns = predicted_churns.filter(created_at__gte=from_date)
-        if to_date:
-            predicted_churns = predicted_churns.filter(created_at__lte=to_date)
-        tutor_comments = TeacherComment.objects.filter(tutor=tutor_profile)
-        churn_comments = []
-        for churn in PredictedChurn.objects.filter(teacher__in=teachers):
-            churn_comments.append((churn.churn_id, tutor_comments.filter(churn_id=churn.churn_id)))
-
-        les_teacher = request.GET.get("les_teacher")
-        if les_teacher:
-            les_teachers = teachers.filter(
-                (Q(user__first_name__icontains=les_teacher) | Q(user__last_name__icontains=les_teacher))
-            )
-        else:
-            les_teachers = teachers
-        groups = Group.objects.filter(teacher_id__in=list(les_teachers.values_list("lms_id", flat=True)),
-                                      type__in=("regular", "individual", "Группа", "Индивидуальная"))
-        les_from_date = request.GET.get("les_date_from")
-        les_from_date = datetime.strptime(les_from_date, "%Y-%m-%d") if les_from_date else datetime.now()
-        les_to_date = request.GET.get("les_date_to")
-        les_to_date = datetime.strptime(les_to_date, "%Y-%m-%d") if les_to_date else les_from_date + timedelta(days=7)
-        open_lessons = []
-        for group in groups:
-            lessons_facade = LessonFacade(lms_service=LMSService, group_id=group.lms_id)
-            lessons_facade.filter_open_lessons()
-            lessons_facade.filter_lessons_by_date(from_date=les_from_date, to_date=les_to_date)
-            for lesson in lessons_facade.lessons:
-                lesson.update({"group_name": group.title,
-                               "group_id": group.lms_id,
-                               "teacher": group.teacher_name,
-                               })
-            open_lessons.extend(lessons_facade.lessons)
     if user_role == "regional_tutor" or user_role == "admin":
         regional_tutor_profile = RegionalTutorProfile.objects.filter(
             user=request.user
