d4a949bfcf0455c68ee026dcb40b5353a2c51a04
Denis Salnikov <denissalnikov@ukr.net>
2025-02-20 14:15:33 +0200

    added str for office region

--- logika_statistics/models.py
+++ logika_statistics/models.py
@@ -30,6 +30,9 @@ regions = [
 class OfficeRegion(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Group(models.Model):
     lms_id = models.IntegerField(unique=True)
